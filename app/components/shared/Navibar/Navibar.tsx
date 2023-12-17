877bb2cac084ee2e8161b3927aecb7baab479806
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-12-17 17:39:25 +0500

    ComponentWrapper com developed

---
+++ app/components/shared/Navibar/Navibar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ComponentWrapper from '../Wrappers/ComponentWrapper/ComponentWrapper';
+
+const Navibar = () => {
+  return <div>Navibar</div>;
+};
+
+export default Navibar;
